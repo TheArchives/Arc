c60b5eb62f538ea151569503972ca95a19fb8175
tyteen4a03 <tyteen4a03@79fbe802-9451-432d-9ef8-bd2fc9be3b1e>
2011-06-23 06:53:40 +0000

    moar derp
    git-svn-id: http://subversion.assembla.com/svn/the-archives/trunk@116 79fbe802-9451-432d-9ef8-bd2fc9be3b1e

--- arc/playerdata.py
+++ arc/playerdata.py
@@ -2,7 +2,7 @@
 # Arc is licensed under the BSD 2-Clause modified License.
 # To view more details, please see the "LICENSING" file in the "docs" folder of the Arc Package.
 
-import os, shutil
+import os, shutil, sys
 from ConfigParser import RawConfigParser as ConfigParser
 
 debug = (True if "--debug" in sys.argv else False)
