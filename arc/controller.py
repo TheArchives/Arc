df030aa347f903488ebefa2f6f60fea697fa475f
tyteen4a03 <tyteen4a03@79fbe802-9451-432d-9ef8-bd2fc9be3b1e>
2011-06-23 09:24:22 +0000

    git-svn-id: http://subversion.assembla.com/svn/the-archives/trunk@122 79fbe802-9451-432d-9ef8-bd2fc9be3b1e

--- arc/controller.py
+++ arc/controller.py
@@ -2,7 +2,7 @@
 # Arc is licensed under the BSD 2-Clause modified License.
 # To view more details, please see the "LICENSING" file in the "docs" folder of the Arc Package.
 
-import logging, traceback
+import sys, traceback
 try:
     import simplejson
 except ImportError:
