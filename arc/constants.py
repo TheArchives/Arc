79a2408bdb273e219daea23e99d7b30194774296
tyteen4a03 <tyteen4a03@79fbe802-9451-432d-9ef8-bd2fc9be3b1e>
2011-07-03 12:36:55 +0000

    Updatning versioning
    git-svn-id: http://subversion.assembla.com/svn/the-archives/trunk@158 79fbe802-9451-432d-9ef8-bd2fc9be3b1e

--- arc/constants.py
+++ arc/constants.py
@@ -1,8 +1,8 @@
 # Arc is copyright 2009-2011 the Arc team and other contributors.
 # Arc is licensed under the BSD 2-Clause modified License.
 # To view more details, please see the "LICENSING" file in the "docs" folder of the Arc Package.
-import arc.version as v
-VERSION = v.version
+import arc.version
+VERSION = "1.0.0_r%s" % SVN_VERSION
 
 INFO_VIPLIST = [
     # Mojang staff (current or retired)
