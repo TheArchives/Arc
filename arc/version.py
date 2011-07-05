22e87ee32c196ed376b222f0143d271296972a3c
gdude2002 <gdude2002@79fbe802-9451-432d-9ef8-bd2fc9be3b1e>
2011-07-05 10:22:49 +0000

    version.py
    git-svn-id: http://subversion.assembla.com/svn/the-archives/trunk@168 79fbe802-9451-432d-9ef8-bd2fc9be3b1e

---
+++ arc/version.py
@@ -0,0 +1,7 @@
+# Arc is copyright 2009-2011 the Arc team and other contributors.
+# Arc is licensed under the BSD 2-Clause modified License.
+# To view more details, please see the "LICENSING" file in the "docs" folder of the Arc Package.
+
+# This file just contains the latest SVN revision number.
+# Please do not edit it.
+SVN_VERSION = 163
