70ba126457c1105492eb2c3ff7aa944942656a01
gdude2002 <gdude2002@79fbe802-9451-432d-9ef8-bd2fc9be3b1e>
2011-07-09 13:12:05 +0000

    systeminfo.py - Added other system info, renamed from cpu.py template.py.disabled - Renamed template.py to disable it
    git-svn-id: http://subversion.assembla.com/svn/the-archives/trunk@176 79fbe802-9451-432d-9ef8-bd2fc9be3b1e

---
+++ arc/serverplugins/systeminfo.py
@@ -0,0 +1,40 @@
+# Arc is copyright 2009-2011 the Arc team and other contributors.
+# Arc is licensed under the BSD 2-Clause modified License.
+# To view more details, please see the "LICENSING" file in the "docs" folder of the Arc Package.
+
+import psutil
+
+class SystemInfoServerPlugin():
+
+    name = "SystemInfoPlugin"
+    errored = False
+
+    def __init__(self, factory):
+        self.factory = factory
+        self.logger = factory.logger
+        
+    def onHeartbeat(self, data=None):
+        cpuall = psutil.cpu_percent(interval=0)
+        cpucores = psutil.cpu_percent(interval=0, percpu=True)
+        cores = len(cpucores)
+        diskusage = psutil.disk_usage("/")
+        physramusage = psutil.phymem_usage()
+        virtramusage = psutil.virtmem_usage()
+        processes = len(psutil.get_pid_list())
+        self.logger.info("CPU USAGE: %s%%, DISK USAGE: %s%% RAM USAGE: %s%% physical, %s%% virtual, PROCESSES: %s" % (cpuall, diskusage[3], physramusage[3], virtramusage[3], processes))
+        if cores == 1:
+            self.logger.debug("CPU: %s%% (in one core)" % cpuall)
+        elif cores > 1:
+            done = ""
+            i = 1
+            for element in cpucores:
+                done = done + ("%s: %s%%, " % (i, element))
+                i = i + 1
+            done = done[0:(len(done)-2)]
+            self.logger.debug("CPU: %s%% (%s)" % (cpuall, done))
+        
+    hooks = {
+        "heartbeatSent": onHeartbeat
+    }
+        
+serverPlugin = SystemInfoServerPlugin
