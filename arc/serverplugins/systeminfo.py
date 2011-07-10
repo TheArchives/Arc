0af794ba386a252b364dc683bc495e4aeb956e23
gdude2002 <gdude2002@79fbe802-9451-432d-9ef8-bd2fc9be3b1e>
2011-07-10 11:17:54 +0000

    systeminfo.py - Fixed.
    git-svn-id: http://subversion.assembla.com/svn/the-archives/trunk@186 79fbe802-9451-432d-9ef8-bd2fc9be3b1e

--- arc/serverplugins/systeminfo.py
+++ arc/serverplugins/systeminfo.py
@@ -9,33 +9,27 @@ except ImportError:
 
 class SystemInfoServerPlugin():
 
-    hooks = {
-        "heartbeatSent": onHeartbeat
-    }
-
-    name = "SystemInfoPlugin"
-    errored = False
-
     def __init__(self, factory):
         self.factory = factory
         self.logger = factory.logger
 
     def calculateSystemUsage(self):
         cpuall = psutil.cpu_percent(interval=0)
-        cores = len(cpucores)
+        cores = len(psutil.cpu_percent(interval=0, percpu=True))
         diskusage = psutil.disk_usage("/")
         physramusage = psutil.phymem_usage()
         virtramusage = psutil.virtmem_usage()
         processes = len(psutil.get_pid_list())
-        theDict = {cpuall, cores, diskusage[3], physramusage[3], virtramusage[3], processes}
-        return theDict
+        theList = [cpuall, cores, diskusage[3], physramusage[3], virtramusage[3], processes]
+        return theList
 
     def onHeartbeat(self, data=None):
-        usageDict = self.calculateSystemUsage
+        usageList = self.calculateSystemUsage()
         cpucores = psutil.cpu_percent(interval=0, percpu=True)
-        self.logger.info("CPU USAGE: %s%% (%s cores), DISK USAGE: %s%%, RAM USAGE: %s%% physical, %s%% virtual, PROCESSES: %s" % (usageDict))
+        cores = len(psutil.cpu_percent(interval=0, percpu=True))
+        self.logger.info("CPU USAGE: %s%% (%s cores), DISK USAGE: %s%%, RAM USAGE: %s%% physical, %s%% virtual, PROCESSES: %s" % (usageList[0], usageList[1], usageList[2], usageList[3], usageList[4], usageList[5]))
         if cores == 1:
-            self.logger.debug("CPU: %s%% (in one core)" % usageDict[0])
+            self.logger.debug("CPU: %s%% (in one core)" % usageList[0])
         elif cores > 1:
             done = ""
             i = 1
@@ -43,6 +37,13 @@ class SystemInfoServerPlugin():
                 done = done + ("%s: %s%%, " % (i, element))
                 i = i + 1
             done = done[0:(len(done)-2)]
-            self.logger.debug("CPU: %s%% (%s)" % (usageDict[0], done))
+            self.logger.debug("CPU: %s%% (%s)" % (usageList[0], done))
+    
+    name = "SystemInfoPlugin"
+    errored = False
+    
+    hooks = {
+        "heartbeatSent": onHeartbeat
+    }
 
 serverPlugin = SystemInfoServerPlugin
