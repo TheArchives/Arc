34d9d1b70bd0649d4eb147e9ac566cad32d89d6e
tyteen4a03 <tyteen4a03@79fbe802-9451-432d-9ef8-bd2fc9be3b1e>
2011-07-10 20:11:58 +0000

    Fixed /sinfo
    git-svn-id: http://subversion.assembla.com/svn/the-archives/trunk@189 79fbe802-9451-432d-9ef8-bd2fc9be3b1e

--- arc/plugins/devtools.py
+++ arc/plugins/devtools.py
@@ -13,5 +13,5 @@ class DevToolsPlugin(ProtocolPlugin):
     }
 
     def commandServerInfo(self, parts, fromloc, overriderank):
-        usageDict = self.client.factory.serverPlugins["SystemInfoPlugin"].calculateSystemUsage()
-        self.client.sendServerMessage("CPU USAGE: %s%% (%s cores), DISK USAGE: %s%%, RAM USAGE: %s%% physical, %s%% virtual, PROCESSES: %s" % (usageDict))
+        usageList = self.client.factory.serverPlugins["SystemInfoPlugin"].calculateSystemUsage()
+        self.client.sendServerMessage("CPU USAGE: %s%% (%s cores), DISK USAGE: %s%%, RAM USAGE: %s%% physical, %s%% virtual, PROCESSES: %s" % (usageList[0], usageList[1], usageList[2], usageList[3], usageList[4], usageList[5]))
