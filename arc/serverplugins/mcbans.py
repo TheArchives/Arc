3f8783c7e518bbb7849e522df835b42db583a4aa
gdude2002 <gdude2002@79fbe802-9451-432d-9ef8-bd2fc9be3b1e>
2011-07-13 11:48:06 +0000

    mcbans.py and mcbans.conf - Added ban threshold and exceptions list
    git-svn-id: http://subversion.assembla.com/svn/the-archives/trunk@207 79fbe802-9451-432d-9ef8-bd2fc9be3b1e

--- arc/serverplugins/mcbans.py
+++ arc/serverplugins/mcbans.py
@@ -26,6 +26,15 @@ class McBansServerPlugin():
             self.logger.info("[&1MCBans&f] MCBans enabled!")
             self.handler = McBans(api_key)
             self.has_api = True
+        try:
+            self.threshold = config.getint("mcbans", "ban_threshold")
+        except Exception as a:
+            self.logger.error("[&1MCBans&f] Unable to get the ban threshold, ignoring global ban reputations!")
+            self.logger.error("[&1MCBans&f] %s" % a)
+            self.threshold = 0
+        else:
+            self.logger.info("[&1MCBans&f] Ban threshold is %s/10" % self.threshold) 
+        self.exceptions = config.options("exceptions")
         del config
         
     def onlineLookup(self, data):
@@ -34,10 +43,17 @@ class McBansServerPlugin():
             data = self.handler.lookup(client.username)
             if int(data["total"]) > 0:
                 self.logger.warn("User %s has &4%s&f bans on record at MCBans and a reputation of %s/10." % (client.username, data["total"], data["reputation"]))
-                client.sendServerMessage("[%sMCBans%s] You have %s%s%s bans on MCBans!" % (COLOUR_BLUE, COLOUR_YELLOW, COLOUR_RED, data["total"], COLOUR_YELLOW))
+                client.sendServerMessage("[%sMCBans%s] You have %s%s%s bans on MCBans and a reputation of %s/10" % (COLOUR_BLUE, COLOUR_YELLOW, COLOUR_RED, data["total"], COLOUR_YELLOW, data["reputation"]))
             else:
                 self.logger.info("User %s has &ano&f bans on record at MCBans and a reputation of %s/10." % (client.username, data["reputation"]))
                 client.sendServerMessage("[%sMCBans%s] You have %sno%s bans on MCBans!" % (COLOUR_BLUE, COLOUR_YELLOW, COLOUR_GREEN, COLOUR_YELLOW))
+            if int(data["reputation"]) < self.threshold:
+                if client.username not in self.exceptions:
+                    self.logger.info("[%sMCBans%s] Kicking %s because their reputation of %s/10 is below the threshold!" % (client.username, data["reputation"]))
+                    client.sendError("Your MCBans reputation of %s/10 is too low!" % data["reputation"])
+                else:
+                    self.logger.info("[%sMCBans%s] %s has a reputation of %s/10 which is below the threshold, but is on the exceptions list." % (client.username, data["reputation"]))
+                
         
     name = "McBansServerPlugin"
     
