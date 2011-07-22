87efb2e80e4893c31e02cebe633e4e9d4e4e1447
tyteen4a03 <tyteen4a03@79fbe802-9451-432d-9ef8-bd2fc9be3b1e>
2011-07-22 14:32:49 +0000

    derp
    git-svn-id: http://subversion.assembla.com/svn/the-archives/trunk@284 79fbe802-9451-432d-9ef8-bd2fc9be3b1e

--- arc/plugins/playerutil.py
+++ arc/plugins/playerutil.py
@@ -385,7 +385,7 @@ class PlayerUtilPlugin(ProtocolPlugin):
                 if len(self.client.factory.worlds[key].clients) > 0:
                     theList = [(key + ": ")]
                     for c in self.client.factory.worlds[key].clients:
-                        user = str(self.client.factory.worlds[key].clients[c].username)
+                        user = self.client.factory.worlds[key].clients[c].username
                         if user is None:
                             pass # To avoid NoneType error
                         else:
