c6fa5c57fc106d35191a4d4825aa03462d4a3915
gdude2002 <gdude2002@79fbe802-9451-432d-9ef8-bd2fc9be3b1e>
2011-07-23 18:51:38 +0000

    server.py - Derp.
    git-svn-id: http://subversion.assembla.com/svn/the-archives/trunk@298 79fbe802-9451-432d-9ef8-bd2fc9be3b1e

--- arc/server.py
+++ arc/server.py
@@ -2,8 +2,7 @@
 # Arc is licensed under the BSD 2-Clause modified License.
 # To view more details, please see the "LICENSING" file in the "docs" folder of the Arc Package.
 
-import ctypes, datetime, gc, os, platform, random, re, shutil, subprocess, sys, time, traceback, cPickle
-import string.printable
+import ctypes, datetime, gc, os, platform, random, re, shutil, subprocess, sys, time, traceback, cPickle, string
 from ConfigParser import RawConfigParser as ConfigParser
 from Queue import Queue, Empty
 
