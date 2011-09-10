63ecc026c5d440361c6cb73e800e09a71be7898c
tyteen4a03 <tyteen4a03@79fbe802-9451-432d-9ef8-bd2fc9be3b1e>
2011-09-10 16:49:03 +0000

    Reverted changes (I have no idea what's happening)
    git-svn-id: http://subversion.assembla.com/svn/the-archives/trunk@371 79fbe802-9451-432d-9ef8-bd2fc9be3b1e

--- arc/serverplugins/asd.py
+++ arc/serverplugins/asd.py
@@ -2,25 +2,33 @@
 # Arc is licensed under the BSD 2-Clause modified License.
 # To view more details, please see the "LICENSING" file in the "docs" folder of the Arc Package.
 
+from ConfigParser import RawConfigParser as ConfigParser
+from collections import defaultdict
+
 from twisted.internet.task import LoopingCall
 
 class AutoShutdownServerPlugin():
 
     name = "AutoShutdownPlugin"
 
-    def __init__(self, factory):
-        self.factory = factory
-        self.logger = factory.logger
-        self.loop = LoopingCall(checkWorld)
-        self.times = dict()
-        
+    def gotServer(self):
+        self.loop = LoopingCall(self.checkWorlds) # TODO: Register this loop into the central loop-de-loop directory
+        self.times = defaultdict(int)
+
+    def runLoop(self):
+        self.loop.start(60) # Check worlds every minute
+
     def checkWorlds(self):
         # Check the worlds
-        if self.client.factory.worlds.keys() != list("default"): # We don't care about default :P
-            
+        if self.factory.worlds.keys() != list("default"): # We don't care about default :P
+            for world in self.factory.worlds.keys():
+                if self.factory.worlds[world].autoshutdown:
+                    if self.times[world] >= self.factory.asd_delay and not self.factory.worlds[world]:
+                        self.factory.unloadWorld(world)
+                    else:
+                        self.times[world] += 1
 
     hooks = {
-        "onWorldMetaLoaded": onWorldMetaLoaded
-    }
-
+        "configLoaded": runLoop
+     }
 serverPlugin = AutoShutdownServerPlugin
