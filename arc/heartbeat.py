fbdeae430b1280f66182fd48f5f0193d84238af4
tyteen4a03 <tyteen4a03@79fbe802-9451-432d-9ef8-bd2fc9be3b1e>
2011-09-17 06:05:29 +0000

    Removed unnecessary module importing
    git-svn-id: http://subversion.assembla.com/svn/the-archives/trunk@381 79fbe802-9451-432d-9ef8-bd2fc9be3b1e

--- arc/heartbeat.py
+++ arc/heartbeat.py
@@ -2,7 +2,7 @@
 # Arc is licensed under the BSD 2-Clause modified License.
 # To view more details, please see the "LICENSING" file in the "docs" folder of the Arc Package.
 
-import sys, threading, time, traceback, urllib, urllib2
+import sys, threading, time, traceback, urllib
 
 from twisted.internet import reactor
 from twisted.internet.task import LoopingCall
