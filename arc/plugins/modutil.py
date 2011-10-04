eb4f146517ce1a77f30061033d8d6bacff6356df
tyteen4a03 <tyteen4a03@79fbe802-9451-432d-9ef8-bd2fc9be3b1e>
2011-10-04 10:31:48 +0000

    -Fixed /title
    git-svn-id: http://subversion.assembla.com/svn/the-archives/trunk@423 79fbe802-9451-432d-9ef8-bd2fc9be3b1e

--- arc/plugins/modutil.py
+++ arc/plugins/modutil.py
@@ -2,6 +2,8 @@
 # Arc is licensed under the BSD 2-Clause modified License.
 # To view more details, please see the "LICENSING" file in the "docs" folder of the Arc Package.
 
+import cPickle
+
 from arc.constants import *
 from arc.decorators import *
 from arc.plugins import *
