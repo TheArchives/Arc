6374c1e1e267482a8a08e4ab3c4c355b99a5eabd
GameMaker Gm <gamemakergm@safaree.co.ccgit config --global user.email gamemakergm@safaree.co.ccgit config --global github.user gamemakergmgit config --global github.token f4b3b701f1d7445bcd18892f55d9d4edmkdir ~/Arccd ~/Arcgit initgit inittouch READMEgit add READMEstopasdsadgit help gitasd Emd>
2011-10-17 21:09:35 -0600

    Test Meep

--- trunk/start.bat
+++ trunk/start.bat
@@ -8,11 +8,16 @@ if "%debug%"=="y" goto debug
 if "%debug%"=="Y" goto debug
 if "%debug%"=="n" goto nodebug
 if "%debug%"=="N" goto nodebug
+if "%debug%"=="Meep" goto meep
 :debug
 python run.py --debug -OO
 goto quit
 :nodebug
 python run.py -OO
+goto quit
+:Meep
+python run.py -OO
+echo Meep
 goto quit
 :quit
 PAUSE
