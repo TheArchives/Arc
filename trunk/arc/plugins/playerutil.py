194c81c106399b505beb50a5174c5b8b013b8443
tyteen4a03 <tyteen4a03@hk-diy.net>
2011-10-29 21:48:37 +0800

    -Fixed /derankname

--- trunk/arc/plugins/playerutil.py
+++ trunk/arc/plugins/playerutil.py
@@ -248,7 +248,7 @@ class PlayerUtilPlugin(ProtocolPlugin):
             else:
                 rank = parts[0]
             rank = rank.strip("/de")
-            parts = ["/derank", parts[1], parts[0]] + ([parts[2]] if len(parts) == 3 else [])
+            parts = ["/derank", parts[1], rank] + ([parts[2]] if len(parts) == 3 else [])
             self.client.sendServerMessage(DeRank(self, parts, fromloc, overriderank))
 
     @config("category", "player")
