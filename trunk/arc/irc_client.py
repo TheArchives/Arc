192631adacebc440e3dc14f69423627f4d092111
tyteen4a03 <tyteen4a03@hk-diy.net>
2011-11-05 22:24:13 +0800

    -Fixed $lastseen

--- trunk/arc/irc_client.py
+++ trunk/arc/irc_client.py
@@ -2,7 +2,7 @@
 # Arc is licensed under the BSD 2-Clause modified License.
 # To view more details, please see the "LICENSING" file in the "docs" folder of the Arc Package.
 
-import cmath, datetime, random, sys, traceback
+import cmath, datetime, random, sys, time, traceback
 
 from twisted.internet import protocol
 from twisted.words.protocols import irc
